e9a4497a94ba42b8642a921e9fceb827e2d20b4f
Du Campe De Rosamel Claire <P5BOPG@ad.insee.intra>
2023-03-07 17:15:38 +0100

    graphs slides

--- Conj/mod2_2019.R
+++ Conj/mod2_2019.R
@@ -23,20 +23,17 @@ saveRDS(sum, file = "summary.RDS")
 
 hansen.test(mod2_2019)
 
-test = rmse_prev(mod2_2019, fixed_bw = TRUE)
-
-piecereg = piece_reg(mod2_2019, break_dates = 2011.25)
-soos_piece = soos_prev(piecereg)
+test = rmse_prev(mod2_2019, fixed_bw = TRUE, break_dates = 2011.25)
 
 test2 = ssm_lm(mod2_2019, var_intercept = 0.1, var_variables = 0.1, fixed_intercept = FALSE, fixed_variables = FALSE)
 
 test3 = ssm_lm_oos(mod2_2019,  var_intercept = 0.1, var_variables = 0.1, fixed_intercept = FALSE, fixed_variables = FALSE)
 
 plot_all = dygraph(cbind(PIB = get_data(mod2_2019)[,1],
-              lm = test$prevision$prev_lm$prevision,
-              piecelm = test$prevision$prev_piece_lm$prevision,
-              tvlm = test$prevision$prev_tvlm$prevision,
-              ssm = test3$prevision))%>%
+                         lm = test$prevision$prev_lm$prevision,
+                         piecelm = test$prevision$prev_piece_lm$prevision,
+                         tvlm = test$prevision$prev_tvlm$prevision,
+                         ssm = test3$prevision))%>%
   dyOptions(colors = c("black", "orange", "green", "red", "purple" ))
 saveRDS(plot_all, file = "graphs_atelier/plot_all.RDS")
 
@@ -45,58 +42,104 @@ saveRDS(plot_all, file = "graphs_atelier/plot_all.RDS")
 data_plot = data.frame(time(get_data(mod2_2019)),
                        get_data(mod2_2019)[,1],
                        mod2_2019$fitted.values,
-                       piecereg$model$fitted.values,
+                       test$model$piece_lm$model$fitted.values,
                        test$model$tvlm$fitted,
                        test2$fitted[, "smoothed"])
 colnames(data_plot) = c("Time", "PIB", "lm", "piecelm", "tvlm", "ssm")
 
-plot_pib = dygraph(data_plot$PIB, main = "PIB entre 1990 et 2019", xlab = "Time") %>%
-  dyOptions(colors = c("black"))
+plot_pib = dygraph(data_plot$PIB, main = "PIB entre 1990 et 2019") %>%
+  dyOptions(colors = c("black"))%>%
+  dyRangeSelector() %>%
+  dySeries("V1", label = "PIB") %>%
+  dyLegend(show = "always", width = 130)
+
+plot_lm = dygraph(cbind(PIB = data_plot$PIB, lm = data_plot$lm), main = "Fitted values modèle linéaire") %>%
+  dyOptions(colors = c("black", "orange"))%>%
+  dyRangeSelector() %>%
+  dyLegend(width = 130)
+
+plot_piecelm = dygraph(cbind(PIB = data_plot$PIB, lm = data_plot$lm, piecelm = data_plot$piecelm), main = "Fitted values régression par morceaux") %>%
+  dyOptions(colors = c("black", "orange", "green"))%>%
+  dyRangeSelector() %>%
+  dyLegend(width = 130)
+
+plot_tvlm = dygraph(cbind(PIB = data_plot$PIB, lm = data_plot$lm, tvlm = data_plot$tvlm), main = "Fitted values régression locale") %>%
+  dyOptions(colors = c("black", "orange", "blue"))%>%
+  dyRangeSelector() %>%
+  dyLegend(width = 130)
+
+plot_ssm = dygraph(cbind(PIB = data_plot$PIB, lm = data_plot$lm, ssm = data_plot$ssm), main = "Fitted values modèle espace-état") %>%
+  dyOptions(colors = c("black", "orange", "purple"))%>%
+  dyRangeSelector() %>%
+  dyLegend(width = 130)
+
 saveRDS(plot_pib, file = "graphs_atelier/plot_pib.RDS")
-
-plot_lm = dygraph(cbind(PIB = data_plot$PIB, lm = data_plot$lm), main = "Fitted values linear model", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange"))
 saveRDS(plot_lm, file = "graphs_atelier/plot_lm.RDS")
-
-plot_piecelm = dygraph(cbind(PIB = data_plot$PIB, lm = data_plot$lm, piecelm = data_plot$piecelm), main = "Fitted values piecewise regression", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "green"))
 saveRDS(plot_piecelm, file = "graphs_atelier/plot_piecelm.RDS")
-
-plot_tvlm = dygraph(cbind(PIB = data_plot$PIB, lm = data_plot$lm, tvlm = data_plot$tvlm), main = "Fitted values locale regression", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "blue"))
 saveRDS(plot_tvlm, file = "graphs_atelier/plot_tvlm.RDS")
-
-plot_ssm = dygraph(cbind(PIB = data_plot$PIB, lm = data_plot$lm, ssm = data_plot$ssm), main = "Fitted values state space model", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "purple"))
 saveRDS(plot_ssm, file = "graphs_atelier/plot_ssm.RDS")
 
-
 data_plot2 = cbind(time(get_data(mod2_2019)),
-                                 get_data(mod2_2019)[,1],
-                                 test$prevision$prev_lm$prevision,
-                                 soos_piece$prevision,
-                                 test$prevision$prev_tvlm$prevision,
-                                 test3$prevision)
+                   get_data(mod2_2019)[,1],
+                   test$prevision$prev_lm$prevision,
+                   test$prevision$prev_piece_lm$prevision,
+                   test$prevision$prev_tvlm$prevision,
+                   test3$prevision)
 colnames(data_plot2) = c("Time", "PIB", "lm", "piecelm", "tvlm", "ssm")
 
-oos_lm = dygraph(cbind(PIB = data_plot2[,"PIB"], lm = data_plot2[,"lm"]), main = "Previsions linear model", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange"))
+oos_lm = dygraph(cbind(PIB = data_plot2[,"PIB"], lm = data_plot2[,"lm"]), main = "Estimations en temps réel modèle linéaire") %>%
+  dyOptions(colors = c("black", "orange"))%>%
+  dyRangeSelector() %>%
+  dyLegend(width = 130)
+
+oos_piecelm = dygraph(cbind(PIB = data_plot2[,"PIB"], lm = data_plot2[,"lm"], piecelm = data_plot2[,"piecelm"]), main = "Estimations en temps réel régression par morceaux") %>%
+  dyOptions(colors = c("black", "orange", "green"))%>%
+  dyRangeSelector() %>%
+  dyLegend(width = 130)
+
+oos_tvlm = dygraph(cbind(PIB = data_plot2[,"PIB"], lm = data_plot2[,"lm"], tvlm = data_plot2[,"tvlm"]), main = "Estimations en temps réel régression locale") %>%
+  dyOptions(colors = c("black", "orange", "blue"))%>%
+  dyRangeSelector() %>%
+  dyLegend(width = 130)
+
+oos_ssm = dygraph(cbind(PIB = data_plot2[,"PIB"], lm = data_plot2[,"lm"], ssm = data_plot2[,"ssm"]), main = "Prévisions en temps réel modèle espace-état") %>%
+  dyOptions(colors = c("black", "orange", "purple")) %>%
+  dyRangeSelector() %>%
+  dyLegend(width = 130)
+
 saveRDS(oos_lm, file = "graphs_atelier/oos_lm.RDS")
-
-oos_piecelm = dygraph(cbind(PIB = data_plot2[,"PIB"], lm = data_plot2[,"lm"], piecelm = data_plot2[,"piecelm"]), main = "Previsions piecewise regression", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "green"))
 saveRDS(oos_piecelm, file = "graphs_atelier/oos_piecelm.RDS")
-
-oos_tvlm = dygraph(cbind(PIB = data_plot2[,"PIB"], lm = data_plot2[,"lm"], tvlm = data_plot2[,"tvlm"]), main = "Previsions locale regression", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "blue"))
 saveRDS(oos_tvlm, file = "graphs_atelier/oos_tvlm.RDS")
-
-oos_ssm = dygraph(cbind(PIB = data_plot2[,"PIB"], lm = data_plot2[,"lm"], ssm = data_plot2[,"ssm"]), main = "Previsions state space model", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "purple"))
 saveRDS(oos_ssm, file = "graphs_atelier/oos_ssm.RDS")
 
 
+dygraph(ts(test$model$tvlm$coefficients, end = 2019.75, frequency = 4))
+dygraph(test$model$lm$coefficients)
+dygraph(test2$smoothed_states[, c(1:3)])
+nrow(data_pib_2019)
 
+coef_tvlm = ts(test$model$tvlm$coefficients, end = 2019.75, frequency = 4)
+coef_ssm = test2$smoothed_states[, c(1:3)]
+coef_lm = ts(cbind(rep(test$model$lm$coefficients[1], 120), test$model$lm$coefficients[2], test$model$lm$coefficients[3]), end = 2019.75, frequency = 4)
+colnames(coef_lm) = colnames(coef_tvlm)
+
+coef_intercept = cbind(lm = coef_lm[,1], int_tvlm = coef_tvlm[,1], int_ssm = coef_ssm[,1])
+coef_climat = cbind(cli_lm = coef_lm[,2], cli_tvlm = coef_tvlm[,2], cli_ssm = coef_ssm[,2])
+coef_diff = cbind(diff_lm = coef_lm[,3], diff_tvlm = coef_tvlm[,3], diff_ssm = coef_ssm[,3])
+
+dygraph(cbind(coef_intercept, coef_climat, coef_diff))
+
+dygraph(coef_intercept)
+dygraph(coef_climat)
+dygraph(coef_diff)
+
+plot_coef_tvlm = dygraph(coef_tvlm, main = "Coefficients régression locale") %>%
+  dyLegend(width = 110) %>%
+  dySeries("(Intercept)", label = "cste") %>%
+  dySeries("climat_fr_m2", label = "climat") %>%
+  dySeries("diff_climat_fr_m2", label = "diff_climat") %>%
+  dyRangeSelector()
+saveRDS(plot_coef_tvlm, file = "graphs_atelier/plot_coef_tvlm.RDS")
 
 
 
--- Conj/mod2_2022.R
+++ Conj/mod2_2022.R
@@ -7,20 +7,21 @@ soldes_trim <- trimestrialise(climat_fr)
 
 pib = (pib / stats::lag(pib, -1)-1)*100
 
-data_pib_90 <- ts.union(pib, soldes_trim, diff(soldes_trim, 1))
-colnames(data_pib_90) <- c("pib", sprintf("climat_fr_m%i", 1:3),
+data_pib_22 <- ts.union(pib, soldes_trim, diff(soldes_trim, 1))
+colnames(data_pib_22) <- c("pib", sprintf("climat_fr_m%i", 1:3),
                         sprintf("diff_climat_fr_m%i", 1:3))
 
-data_pib_90 <- window(data_pib_90, start = 1990, end = c(2022,4)
-)
+data_pib_23 <- window(data_pib_22, start = 2023, end = 2023)
+data_pib_23 <- c("(Intercept)" = 1, data_pib_23[, c("climat_fr_m2", "diff_climat_fr_m2")])
+data_pib_22 <- window(data_pib_22, start = 1990, end = c(2022,4))
 
-model <- dynlm::dynlm(pib ~ climat_fr_m2 + diff_climat_fr_m2, data = data_pib_90)
+model <- dynlm::dynlm(pib ~ climat_fr_m2 + diff_climat_fr_m2, data = data_pib_22)
 
-ind <- cbind(time(data_pib_90) == 2020, time(data_pib_90) == 2020.25, time(data_pib_90) == 2020.5,
-             time(data_pib_90) == 2020.75)
-ind <- ts(apply(ind,2, as.numeric), start = start(data_pib_90), frequency = 4)
-data3 <- cbind(data_pib_90, ind)
-colnames(data3) <- c(colnames(data_pib_90), sprintf("ind2020Q%i", 1:4))
+ind <- cbind(time(data_pib_22) == 2020, time(data_pib_22) == 2020.25, time(data_pib_22) == 2020.5,
+             time(data_pib_22) == 2020.75)
+ind <- ts(apply(ind,2, as.numeric), start = start(data_pib_22), frequency = 4)
+data3 <- cbind(data_pib_22, ind)
+colnames(data3) <- c(colnames(data_pib_22), sprintf("ind2020Q%i", 1:4))
 
 model_ind <-dynlm::dynlm(pib ~ climat_fr_m2 + diff_climat_fr_m2 +
                           ind2020Q1 + ind2020Q2 + ind2020Q3 + ind2020Q4,
@@ -28,47 +29,36 @@ model_ind <-dynlm::dynlm(pib ~ climat_fr_m2 + diff_climat_fr_m2 +
 
 data3 = cbind(pib_cor = data3[,"pib"] - rowSums(ind * coef(model_ind)[4:7]),
               data3)
-colnames(data3) = c("pib_cor", colnames(data_pib_90), sprintf("ind2020Q%i", 1:4))
+colnames(data3) = c("pib_cor", colnames(data_pib_22), sprintf("ind2020Q%i", 1:4))
 
 model_cor <- dynlm::dynlm(pib_cor ~ climat_fr_m2 + diff_climat_fr_m2,
                          data = data3)
 
-rmse = rmse_prev(model_cor, fixed_bw = TRUE, var_fixes = fixed_coefficients(model_cor))
-sort(rmse$rmse[[1]])
-sort(rmse$rmse[[2]])
+rmse = rmse_prev(model_cor, fixed_bw = TRUE, break_dates = 2011.25)
 
-x = piece_reg(model_cor, break_dates = 2011)
-summary(x$model)
-x$model$residuals %>% rmse_res
-soos_x = soos_prev(x$model)
+ssm = ssm_lm(model_cor, var_intercept = 0.1, var_variables = 0.1, fixed_intercept = FALSE, fixed_variables = FALSE)
 
-dygraph(cbind(PIB = get_data(model_cor)[,1],
-              lm = rmse$model$lm$fitted.values,
-              tvlm = rmse$model$tvlm$fitted))%>%
-  dyOptions(colors = c("black", "orange", "red"))
+ssm_oos = ssm_lm_oos(model_cor, var_intercept = 0.1, var_variables = 0.1, fixed_intercept = FALSE, fixed_variables = FALSE)
 
-dygraph(get_data(model_cor)[,1], main = "PIB entre 1990 et 2022", xlab = "Time") %>%
-  dyOptions(colors = c("black"))
+last_coef_lm = last_coef(rmse$model$lm)
+last_coef_tvlm = last_coef(rmse$model$tvlm)
+last_coef_piece = last_coef(rmse$model$piece_lm$model)[4:6]
+last_coef_ssm = tail(ssm$smoothed_states,1)[1:3]
 
-dygraph(cbind(PIB = get_data(model_cor)[,1], lm = rmse$model$lm$fitted.values), main = "Fitted values linear model", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange"))
+prev_23_lm = sum(data_pib_23*last_coef_lm)
+prev_23_tvlm = sum(data_pib_23*last_coef_tvlm)
+prev_23_piece = sum(data_pib_23*last_coef_piece)
+prev_23_ssm = sum(data_pib_23*last_coef_ssm)
 
-dygraph(cbind(PIB = get_data(model_cor)[,1], lm = rmse$model$lm$fitted.values, piecelm = rmse$model$piece_lm$fitted.values), main = "Fitted values piecewise regression", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "green"))
+plot_previsions = dygraph(cbind(PIB = get_data(model_cor)[,1],
+           lm = ts(c(rmse$prevision$prev_lm$prevision, prev_23_lm), end = 2023, frequency = 4),
+           piece_lm = ts(c(rmse$prevision$prev_piece_lm$prevision, prev_23_piece), end = 2023, frequency = 4),
+           tvlm = ts(c(rmse$prevision$prev_tvlm$prevision, prev_23_tvlm), end = 2023, frequency = 4),
+           ssm = ts(c(ssm_oos$prevision, prev_23_ssm), end = 2023, frequency = 4)),
+        main = "Previsions m2 2023 Q1") %>%
+  dyOptions(colors = c("black", "orange", "green", "blue", "purple")) %>%
+  dyRangeSelector(dateWindow = c("2018-01-01", "2023-01-01")) %>%
+  dyLegend(width = 130)
+saveRDS(plot_previsions, file = "graphs_atelier/plot_previsions.RDS")
 
-dygraph(cbind(PIB = get_data(model_cor)[,1], lm = rmse$model$lm$fitted.values, tvlm = rmse$model$tvlm_fixe$fitted), main = "Fitted values locale regression", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "blue"))
 
-rmse2 = rmse_prev(model_ind, fixed_bw = TRUE)
-
-dygraph(get_data(model_ind)[,1], main = "PIB entre 1990 et 2022", xlab = "Time") %>%
-  dyOptions(colors = c("black"))
-
-dygraph(cbind(PIB = get_data(model_ind)[,1], lm = rmse2$model$lm$fitted.values), main = "Fitted values linear model", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange"))
-
-dygraph(cbind(PIB = get_data(model_ind)[,1], lm = rmse2$model$lm$fitted.values, piecelm = rmse2$model$piece_lm$model$fitted.values), main = "Fitted values piecewise regression", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "green"))
-
-dygraph(cbind(PIB = get_data(model_ind)[,1], lm = rmse2$model$lm$fitted.values, tvlm = rmse2$model$tvlm$fitted), main = "Fitted values locale regression", xlab = "Time") %>%
-  dyOptions(colors = c("black", "orange", "blue"))
